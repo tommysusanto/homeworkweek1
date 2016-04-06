91fd47e3e92924d9099be24092adbcab7008bdf7
Tommy Susanto <tommy88susanto@gmail.com>
2016-04-06 22:33:53 +1000

    init

---
+++ homework1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by Tommy Susanto on 6/04/2016.
+//  Copyright © 2016 Tommy Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
