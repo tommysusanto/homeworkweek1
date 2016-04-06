3fb8f6a158949c0076c9bd47adf66d7e48e5255e
Tommy Susanto <tommy88susanto@gmail.com>
2016-04-06 23:32:07 +1000

    Add Segue

--- homework1/ViewController.swift
+++ homework1/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUser: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        var DestViewController : ViewTwo = segue.destinationViewController as! ViewTwo
+        
+        DestViewController.lblText = txtUser.text!
+        
     }
 
 
---
+++ homework1/ViewTwo.swift
@@ -0,0 +1,29 @@
+//
+//  ViewTwo.swift
+//  homework1
+//
+//  Created by Tommy Susanto on 6/04/2016.
+//  Copyright © 2016 Tommy Susanto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewTwo: UIViewController {
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    var lblText = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblWelcome.text = lblText
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
